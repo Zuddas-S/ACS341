ead7965509cf30588421c5c9a6bee2defa5c0767
Seb <szuddas1@sheffield.ac.uk>
2022-02-21 12:28:14 +0000

    Lab1 Exercise 2 Stochastic gradient descent completed. Moving on to mini-batch gradient descent.

--- Lab1/Exercise2.py
+++ Lab1/Exercise2.py
@@ -55,7 +55,6 @@ def plotGradientDescent(theta,eta,thetaPath=None,nIerations=1000):
 
 np.random.seed(42)
 theta=np.random.randn(2,1)#for a random initialisation
-
 plt.figure(figsize=(10,4))#making a figure
 plt.subplot(131);plotGradientDescent(theta,eta=0.02) #we see that learning rate too small takes many iterations - slow
 plt.ylabel("$y$",rotation=0,fontsize=18)#we only need one y on the LHS to represent all of our figs
@@ -63,9 +62,47 @@ plt.subplot(132);plotGradientDescent(theta,eta=0.1,thetaPath=thetaPathBGD)#just 
 plt.subplot(133);plotGradientDescent(theta,eta=0.5)#too high learning rate, line jumps about too much.
 setup.save_fig("Gradient Descent Plot")#save the plot using the funciton made in setup
 plt.show()
-
 ####################################################################################
 #Stochastic gradient descent
+
+thetaPathSGD=[]
+m=len(xb)
+np.random.seed(42)
+
+nEpochs=50
+t0,t1=5,50 # Learning Schedule Hyperparameters
+def learningSchedule(t):
+    return t0/(t+t1)
+theta=np.random.randn(2,1) # random initialisation
+
+for epoch in range(nEpochs):
+    for i in range(m):
+        if epoch==0 and i<20:
+            yPredict=xnewb.dot(theta)
+            style='b-'if i>0 else 'r--' # first iteration make a red dashed line
+            plt.plot(xnew,yPredict,style)
+        randomIndex = np.random.randint(m)
+        xi = xb[randomIndex:randomIndex + 1]
+        yi = y[randomIndex:randomIndex + 1]
+        gradients=2*xi.T.dot(xi.dot(theta)-yi)
+        eta=learningSchedule(epoch*m+i)
+        theta=theta-eta*gradients
+        thetaPathSGD.append(theta)
+
+plt.plot(x,y,'b.')
+plt.xlabel('$x_1$',fontsize=18)
+plt.ylabel('$y$',rotation=0,fontsize=18)
+plt.axis([0,2,0,15])
+setup.save_fig("Stochastic Gradient Descent")
+plt.show()
+##############################################################################################
+#
+
+
+
+
+
+
 
 
 
