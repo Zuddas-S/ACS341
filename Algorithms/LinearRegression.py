6b742a2074b601d20819a69415de2eb5aa0f42d9
MattG-bci <mateusz.f.grzybowski@gmail.com>
2022-03-05 16:36:12 +0000

    Linear Regression for an arbitrary input

--- Algorithms/LinearRegression.py
+++ Algorithms/LinearRegression.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -7,41 +8,46 @@ y = 3*X + 4 + np.random.rand(no_data_points, 1) # Defining the output according 
 X_b = np.append(np.ones((len(X), 1)), X, axis=1) # Adding the bias term.
 
 # Defining the simple linear regression line
-class Regression:
+class LinearRegression:
 
     def __init__(self, n_iter=1000, eta=0.01):
         self.theta = 0
         self.n_iter = n_iter
         self.eta = eta
 
-    def initialize_weights(self):
-        self.theta =  np.random.rand(2,1)*10
-
-    def compute_loss(self, X, y):
-        m = len(X)
-        return 2.0/m * np.dot(X.T, (np.dot(X, self.theta) - y))
-
     def train(self, X, y):
-        self.initialize_weights()
+        self._initialize_weights(X)
         self.error_iter = []
         self.theta_iter = []
         for i in range(self.n_iter):
-            loss = self.compute_loss(X, y)
+            loss = self._compute_loss(X, y)
             self.theta = self.theta - self.eta*loss
             y_hat = np.dot(X, self.theta) # not an actual prediction but necessary for calculation of MSE.
-            error = self.compute_mse(y_hat, y)
+            error = self._compute_mse(y_hat, y)
             self.error_iter.append(error)
             self.theta_iter.append(self.theta)
         return self
+    
+    def predict(self, X):
+        return np.dot(X, self.theta)
 
-    def compute_mse(self, y_hat, y):
+    def _initialize_weights(self, X):
+        n_outputs = X.shape[1]
+        self.theta =  np.random.rand(n_outputs, 1)*10
+
+    def _compute_loss(self, X, y):
+        m = len(X)
+        return 2.0/m * np.dot(X.T, (np.dot(X, self.theta) - y))
+
+    def _compute_mse(self, y_hat, y):
         return np.mean((y_hat - y) ** 2)
 
 # Training the model.
-theta = np.random.rand(2,1)*10
-reg = Regression()
-reg.train(X_b, y)
-params = reg.theta_iter
+model = LinearRegression()
+model.train(X_b, y)
+y_pred = model.predict(np.array([3, 4]))
+print(y_pred)
+params = model.theta_iter
 
 # Visualising the regression line against data.
 fig = plt.figure()
