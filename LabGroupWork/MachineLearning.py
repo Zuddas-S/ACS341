0b4ea6ceb560ed8fd96602fc436e068ccb771334
Seb <szuddas1@sheffield.ac.uk>
2022-03-09 17:01:23 +0000

    Reduced some more dimensions created CSV file. Also started programming the ML python code. Need to clean data as seaborn cannot process '?'.

--- LabGroupWork/MachineLearning.py
+++ LabGroupWork/MachineLearning.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+import sklearn
+
+
+fullDataFrame = pd.read_csv('/Users/seb/PycharmProjects/ACS341/LabGroupWork/DatasetDimensionReduced.csv')
+fullDataFrame.head()
+
+
+sns.scatterplot(x='wind_speed',y='pressure',data=fullDataFrame)
+plt.show()
