8e11308081bc865f03aeef48acd21779c2a42ef8
Seb <szuddas1@sheffield.ac.uk>
2022-03-15 10:23:26 +0000

    Added simple plots

--- LabGroupWork/MachineLearning.py
+++ LabGroupWork/MachineLearning.py
@@ -2,13 +2,18 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 import sklearn
 
+fullDataFrame = pd.read_csv('/Users/seb/PycharmProjects/ACS341/LabGroupWork/Dataset_cleaned.csv')
+print(fullDataFrame)
 
-fullDataFrame = pd.read_csv('/Users/seb/PycharmProjects/ACS341/LabGroupWork/DatasetDimensionReduced.csv')
-fullDataFrame.head()
+print(fullDataFrame.isnull().sum()==0)
 
 
-sns.scatterplot(x='wind_speed',y='pressure',data=fullDataFrame)
+#plotting will not work without clean data. 
+#sns.scatterplot(x='rain_1h',y='pressure',data=fullDataFrame)
+#plt.show()
+
+sns.scatterplot(x='time_of_day',y='temp',hue='temp',data=fullDataFrame)
+#plt.legend(labels=['temp','priceActual'])
 plt.show()
