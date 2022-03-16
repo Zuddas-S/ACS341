8a8da3f12e3f05a1c5ad4b56241bd559e3127ebd
Seb <szuddas1@sheffield.ac.uk>
2022-03-16 20:53:37 +0000

    ML File Changes

--- LabGroupWork/MachineLearning.py
+++ LabGroupWork/MachineLearning.py
@@ -4,15 +4,16 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import sklearn
 
-fullDataFrame = pd.read_csv('/Users/seb/PycharmProjects/ACS341/LabGroupWork/Dataset_cleaned.csv')
-print(fullDataFrame)
+fullDataFrame = pd.read_csv('/Users/seb/PycharmProjects/ACS341/LabGroupWork/Dataset_cleaned_FINAL.csv',low_memory=False)
+fullDataFrame.head()
 
-print(fullDataFrame.isnull().sum()==0)
+#print(fullDataFrame.isnull().sum()==0) test to see whether the data is clean
 
 
 #plotting will not work without clean data. 
 #sns.scatterplot(x='rain_1h',y='pressure',data=fullDataFrame)
 #plt.show()
+
 
 sns.scatterplot(x='time_of_day',y='temp',hue='temp',data=fullDataFrame)
 #plt.legend(labels=['temp','priceActual'])
