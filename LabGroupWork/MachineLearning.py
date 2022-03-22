fc8a944839d98c231b3c043e8b32002fecf25f67
Seb <szuddas1@sheffield.ac.uk>
2022-03-22 11:07:22 +0000

    Splitting dataframes and plotting on multiple plots.

--- LabGroupWork/MachineLearning.py
+++ LabGroupWork/MachineLearning.py
@@ -1,20 +1,42 @@
+from email import header
+from http.cookiejar import FileCookieJar
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import sklearn
+from sklearn.utils import shuffle
 
 fullDataFrame = pd.read_csv('/Users/seb/PycharmProjects/ACS341/LabGroupWork/Dataset_cleaned_FINAL.csv',low_memory=False)
 fullDataFrame.head()
 
-#print(fullDataFrame.isnull().sum()==0) test to see whether the data is clean
+print(fullDataFrame.isnull().sum())# test to see whether the data is clean
+
+energy_data=fullDataFrame[['generationFossilBrownCoal_lignite','generationFossilGas','generationBiomass','generationFossilOil','time_of_day','Epoch_time','humidity','generationSolar','generationWaste','forecastSolarDayAhead','forecastWindOnshoreDayAhead','totalLoadActual','generationWindOnshore','generationOther','generationNuclear','generationHydroRun_of_riverAndPoundage','generationHydroWaterReservoir','totalLoadForecast','generationOtherRenewable','generationHydroPumpedStorageConsumption','generationFossilHardCoal']]
+price_data=fullDataFrame[['priceDayAhead','priceActual']]
+
+print((energy_data))
+
+
+shuffled_data=shuffle(fullDataFrame)
 
 
 #plotting will not work without clean data. 
-#sns.scatterplot(x='rain_1h',y='pressure',data=fullDataFrame)
+#sns.scatterplot(x='rain_1h',y='pressure',data=shuffled_data)
 #plt.show()
+#
 
 
-sns.scatterplot(x='time_of_day',y='temp',hue='temp',data=fullDataFrame)
+"""
+
+
+
+shuffled_data['temp'] = shuffled_data['temp'].astype(float)
+shuffled_data['generationFossilBrownCoal_lignite'] = shuffled_data['generationFossilBrownCoal_lignite'].astype(float)
+
+columns=2
+
+fig,axs=plt.subplots(ncols=columns)#make a figure with 2 columns for two plots
+sns.regplot(x='time_of_day',y='temp',order=3,data=shuffled_data,ax=axs[0])
+sns.scatterplot(x='time_of_day',y='temp',hue='generationFossilBrownCoal_lignite',data=fullDataFrame,ax=axs[1])
 #plt.legend(labels=['temp','priceActual'])
-plt.show()
+plt.show()"""
